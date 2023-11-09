211d349ec5a29836943a5841f7f9e98971ea0bf2
Jiří Zmrhal <jzmrhal@actum.cz>
2023-11-09 14:54:04 +0100

    CSS tweaks

--- popup/Controls/Switch.tsx
+++ popup/Controls/Switch.tsx
@@ -1,18 +1,29 @@
 const Switch = ({
   enabled,
   handleChange
-}) => (
-  <label className="switch__label" htmlFor="switch">
-    Active
-    <input
-      className="switch__input visually-hidden"
-      type="checkbox"
-      id="switch"
-      checked={enabled}
-      onChange={handleChange}
-    />
-    <div className="switch"></div>
-  </label>
-)
+}) => {
+  const handleKeyDown = (e) => {
+    // Space is handled by default on checkbox but Enter is not
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleChange();
+    }
+  }
+
+  return (
+    <label className="switch__label" htmlFor="switch">
+      Active
+      <input
+        className="switch__input visually-hidden"
+        type="checkbox"
+        id="switch"
+        checked={enabled}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <div className="switch"></div>
+    </label>
+  )
+}
 
 export default Switch
